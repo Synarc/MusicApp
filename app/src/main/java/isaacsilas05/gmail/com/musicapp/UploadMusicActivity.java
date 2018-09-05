2ae112730e1476d2d88389d53c1955a85f48a5c4
ikialo <synarcapp@gmail.com>
2018-09-05 10:38:46 +1000

    Upload user interface set up.

---
+++ app/src/main/java/isaacsilas05/gmail/com/musicapp/UploadMusicActivity.java
@@ -0,0 +1,13 @@
+package isaacsilas05.gmail.com.musicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadMusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_music);
+    }
+}
